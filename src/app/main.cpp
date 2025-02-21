1925ba9129340a0529cc99dfbb20a588560383cf
hinata <hinatachikao1971@gmail.com>
2025-02-22 00:05:11 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,48 +1,11 @@
 #include <iostream>
-#include <python3.12/Python.h>
+#include "../header/main.hpp"
 
 
-int main(int argc, char **argv) {
-
-    std::cout << "Number of arguments are: " << argc << '\n';
-    PyStatus status{};
-    PyConfig config{};
-    PyConfig_InitPythonConfig(&config);
-
-    /* optional but recommended */
-    status = PyConfig_SetBytesString(&config, &config.program_name, argv[0]);
-    if (PyStatus_Exception(status)) {
-        goto exception;
-    }
-    PyConfig_Clear(&config);
-
-    /* ------------------------------------------------------ */
-
-    std::clog << "first python invocation ..." << '\n';
-
-    PyRun_SimpleString("from time import time, ctime\n\n"
-                       "if __name__ == \"__main__\":"
-                       "    print(\"To Day is: \", ctime(time()))");
-
-    std::clog << "second python invocation ..." << '\n';
-
-    PyRun_SimpleString("print('Hello Python ...')");
-
-    std::clog << "the end of python process ..." << '\n';
+int main() {
 
 
-    /* ------------------------------------------------------ */
 
-    if (Py_FinalizeEx() < 0) {
-        exit(120);
-    }
-
-    std::clog << "step 3 ..." << '\n';
-
-exception:
-    PyConfig_Clear(&config);
-    Py_ExitStatusException(status);
-
-    std::cout << "\n #(00:00:00): The End ..." << std::endl;
-    return (EXIT_SUCCESS);
+    std::cout << "\n #(00:05:11): The End ..." << std::endl;
+    return EXIT_SUCCESS;
 }
