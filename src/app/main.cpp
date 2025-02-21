9227fa2d3417118ce9e50370258c26de90ac0e81
hinata <hinatachikao1971@gmail.com>
2025-02-22 00:25:48 +0330

    new cpp source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,41 @@
-#include <iostream>
 #include "../header/main.hpp"
+#include <iostream>
+#include <python3.12/Python.h>
 
 
-int main() {
+int main(int argc, char **argv) {
 
+    std::cout << "Number of arguments are: " << argc << '\n';
 
+    // Initialize Python configuration
+    PyStatus status{};
+    PyConfig config{};
+    PyConfig_InitPythonConfig(&config);
+
+    // Optional but recommended
+    status = PyConfig_SetBytesString(&config, &config.program_name, argv[0]);
+    if (PyStatus_Exception(status)) {
+        PyConfig_Clear(&config);
+        Py_ExitStatusException(status);
+        std::cerr << "Error in Python configuration..." << '\n';
+        exit(EXIT_FAILURE);
+    }
+
+    PyConfig_Clear(&config);
+    Py_Initialize();
+
+    // Execute Python code
+    PyRun_SimpleString("from time import time, ctime\n"
+                       "if __name__ == '__main__':\n"
+                       "    print('Today is:', ctime(time()))");
+
+    PyRun_SimpleString("print('Hello Python...')");
+
+    // Finalize the Python interpreter
+    if (Py_FinalizeEx() < 0) {
+        exit(120);
+    }
 
     std::cout << "\n #(00:20:03): The End ..." << std::endl;
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
 }
