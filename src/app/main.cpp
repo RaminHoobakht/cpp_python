1fc141300666988bc07ff972f0da7324808dceb3
hinata <hinatachikao1971@gmail.com>
2025-02-22 03:34:48 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,43 +1,11 @@
 #include <iostream>
-#include <python3.12/Python.h>
+#include "../header/main.hpp"
 
 
-int main(int argc, char **argv) {
-
-    int s{1};
-    PyObject *g{PyDict_New()};
-    PyObject *l{PyDict_New()};
-    constexpr int FEC{120};
-
-    std::cout << "Number of argument are: " << argc << '\n';
-
-    PyStatus status{};
-    PyConfig config{};
-    PyConfig_InitPythonConfig(&config);
-
-    status = PyConfig_SetBytesString(&config, &config.program_name, argv[0]);
-    if (PyStatus_Exception(status)) {
-        PyConfig_Clear(&config);
-        Py_ExitStatusException(status);
-    }
-
-    PyConfig_Clear(&config);
-    Py_Initialize();
-
-    /* ------------------------------------------- */
-
-    PyRun_StringFlags("print('Hello Python ...')", s, g, l, NULL);
+int main() {
 
 
-    /* ------------------------------------------- */
 
-
-    if (Py_FinalizeEx() < 0) {
-        std::cerr << "error in python finalize ..." << '\n';
-        exit(FEC);
-    }
-
-
-    std::cout << "The End ..." << std::endl;
+    std::cout << "\n #(03:34:48): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
