f3f48d0231e4be7d81fb9bb4fd72a81479553366
hinata <hinatachikao1971@gmail.com>
2025-02-24 08:44:43 +0330

    after creating new source file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,49 +1,11 @@
-#include "../header/main.hpp"
 #include <iostream>
-#include <python3.12/Python.h>
-
-int main(int argc, char **argv) {
-
-    std::cout << "number of args are: " << argc << NL;
-    constexpr auto *python_file_name{"src/python/main.py"};
-    constexpr auto *read_mode{"r"};
-
-    FILE *my_file;
-    my_file = fopen(python_file_name, read_mode);
+#include "../header/main.hpp"
 
 
-    PyStatus status{};
-    PyConfig config{};
-    PyConfig_InitPythonConfig(&config);
-
-    /* ------------------------------ */
-
-    status = PyConfig_SetBytesString(&config, &config.program_name, argv[0]);
-    if (PyStatus_Exception(status)) {
-        PyConfig_Clear(&config);
-        Py_ExitStatusException(status);
-    }
-
-    /* ------------------------------ */
-
-    PyConfig_Clear(&config);
-    Py_Initialize();
-
-    /* ------------------------------  */
-
-    // PyRun_SimpleString("print('Hello Python ...')");
-
-    PyRun_SimpleFile(my_file, "src/python/");
-    fclose(my_file);
-
-    /* ------------------------------  */
+int main() {
 
 
-    if (Py_FinalizeEx() < 0) {
-        exit(120);
-    }
 
-
-    std::cout << "\n #(02:01:33): The End ..." << std::endl;
+    std::cout << "\n #(08:44:43): The End ..." << std::endl;
     return EXIT_SUCCESS;
 }
