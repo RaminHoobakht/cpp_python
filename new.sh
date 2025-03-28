b2086399f14238942657d23b780bf644feea231c
hinata <hinatachikao1971@gmail.com>
2025-03-29 01:52:40 +0330

    new changes for shell script and library in header folder

--- new.sh
+++ new.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
 clear
-ls -ls archive
+ls -ltrh archive
 echo "enter file number: "
-read -r file_no
+read -r fileno
 
 git add --all
 git commit -m 'before creating new source file'
 
-mv src/app/main.cpp sample/main_"$file_no".cpp
+mv src/app/main.cpp archive/main_"$fileno".cpp
 
 now="$(date +"%T")"
 
-printf '%s\n' "#include <iostream>
-#include \"../header/main.hpp\"
-#include <python3.12/Python.h>
+printf '%s\n' "/*  */
+
+#include \"../header/utillib.hpp\"
+#include <iostream>
 
 
-int main(int argc, char **argv) {
-
+int main() {
 
 
     std::cout << \"\n #($now): The End ...\" << std::endl;
@@ -26,5 +26,5 @@ int main(int argc, char **argv) {
 }" >> src/app/main.cpp
 
 git add --all
-git commit -m 'after creating new source file'
+git commit -m 'before creating new source file'
 
--- newsrc.sh
+++ newsrc.sh
@@ -18,13 +18,13 @@ mkdir src/header
 
 now="$(date +"%T")"
 
-printf '%s\n' "#include <iostream>
-#include \"../header/main.hpp\"
-#include <python3.12/Python.h>
+printf '%s\n' "/*  */
+
+#include \"../header/utillib.hpp\"
+#include <iostream>
 
 
-int main(int argc, char **argv) {
-
+int main() {
 
 
     std::cout << \"\n #($now): The End ...\" << std::endl;
@@ -35,32 +35,72 @@ printf '%s\n' "#ifndef HEADER_MAIN_CPP
 #define HEADER_MAIN_CPP
 
 #include <iostream>
+#include <string>
 
 #define NL '\n'
+#define RT '\r'
 #define SP '\x20'
+#define LINEFEED std::cout << NL
+#define SEP util::separator()
 
 namespace util {
-
-    constexpr size_t min{0LLU};
+    constexpr size_t zero{0LU};
 
     void separator(size_t, char) noexcept;
 
+    std::string &trim(std::string &str, bool right, bool left) noexcept;
 
-    void separator(const size_t no = 64, const char ch = '-') noexcept {
-        for (size_t i{min}; i < no; ++i) {
+
+    void separator(const size_t no = 64LU,
+                          const char ch = '-') noexcept {
+        for (size_t i{zero}; i < no; ++i) {
             std::cout << ch;
         }
         std::cout << NL;
     }
 
+    std::string &trim(std::string &str, const bool right,
+                             const bool left) noexcept {
 
+        if (right) {
+            while (str[0] < 33) {
+                str.erase(0, 1);
+            }
+        }
+
+        if (left) {
+            while (str[str.size() - 1] < 33) {
+                str.erase(str.size() - 1, 1);
+            }
+        }
+
+        return str;
+    }
+
+
+    /* -------------------------------------------------- */
+
+    class publicexception final : public std::exception {
+    private:
+        std::string message_{};
+
+    public:
+        explicit publicexception(std::string message) :
+            message_(std::move(message)) {}
+
+        // publicexception(const exception &exception, std::string message) :
+        //     std::exception(exception), message_(std::move(message)) {}
+
+        // publicexception(exception &&exception, std::string message) :
+        //     std::exception(exception), message_(std::move(message)) {}
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return this->message_.c_str();
+        }
+    };
 } // namespace util
 
 #endif /* HEADER_MAIN_CPP */" >> src/header/main.hpp
-
-mkdir src/python
-printf '%s\n' "# python script file\n" >> src/python/main.py
-printf '%s\n' "# python log file\n" >> src/python/python.log
 
 git add --all
 git commit -m 'after creating new source file'
