7e6fa0d7fdddae3e04afa33bb9dfdbe6e4b72551
hinata <hinatachikao1971@gmail.com>
2025-04-16 19:42:13 +0330

    new cpp source files

--- src/header/utillib.hpp
+++ src/header/utillib.hpp
@@ -1,7 +1,7 @@
 #ifndef HEADER_MAIN_CPP
 #define HEADER_MAIN_CPP
 
-#include <cstddef>
+#include <ios>
 #include <iostream>
 #include <string>
 
@@ -9,8 +9,20 @@ constexpr char NL{'\n'};
 constexpr char RT{'\r'};
 constexpr char SP{'\x20'};
 
+#define out                                                                    \
+    std::cout << std::fixed;                                                   \
+    std::cout << SP
+#define log                                                                    \
+    std::cout << std::fixed;                                                   \
+    std::clog << SP
+#define err                                                                    \
+    std::cout << std::fixed;                                                   \
+    std::cerr << SP
+
 #define LF std::cout << NL
 #define SEP util::separator()
+#define LF_SEP util::lf_separator()
+#define LF_SEP_LF util::lf_separator_lf()
 
 using str = std::string;
 using cstr = const std::string;
@@ -24,6 +36,8 @@ namespace util {
     /* -------------------------------------------------- */
 
     void separator(size_t no = 64LU, char ch = '-') noexcept;
+    void lf_separator(size_t no = 64LU, char ch = '-') noexcept;
+    void lf_separator_lf(size_t no = 64LU, char ch = '-') noexcept;
 
     std::string &trim(std::string &str, bool right, bool left) noexcept;
 
