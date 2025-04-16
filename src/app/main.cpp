5967ab32f2bb3be7b6c5498f405a22ff8fd727e9
hinata <hinatachikao1971@gmail.com>
2025-04-17 01:38:11 +0330

    new cpp source files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,47 +1,10 @@
-#include "../header/main.hpp"
-#include <iostream>
-#include <python3.12/Python.h>
+/*   */
 
-int main(const int argc, char **argv) {
-    /* Code */
+#include "main.hpp"
 
-    if (argc < 1) {
-        std::cerr << "invalid parameter ..." << NL;
-        exit(EXIT_FAILURE);
-    }
 
-    constexpr auto script_file_name{"src/python/main.py"};
-    constexpr auto log_file_name{"src/python/python.log"};
-    constexpr auto read_mode{"r"};
+int main() {
 
-    FILE *py_file{};
-    py_file = fopen(script_file_name, read_mode);
-    if (!py_file) {
-        perror("error in open file");
-        exit(EXIT_SUCCESS);
-    }
-
-    PyStatus status{};
-    PyConfig config{};
-    PyConfig_InitPythonConfig(&config);
-
-    status = PyConfig_SetBytesString(&config, &config.program_name, argv[0]);
-    if (PyStatus_Exception(status)) {
-        PyConfig_Clear(&config);
-        Py_ExitStatusException(status);
-    }
-
-    PyConfig_Clear(&config);
-    Py_Initialize();
-
-    if (PyRun_SimpleFile(py_file, log_file_name) < 0) {
-        std::cerr << "error in run python script ..." << NL;
-    }
-
-    if (Py_FinalizeEx() < 0) {
-        std::cerr << "error in python finalizing ..." << NL;
-    }
-
-    std::cout << "\nThe End ..." << std::endl;
+    out << "\n #(00:00:00): The End ..." << end;
     return EXIT_SUCCESS;
 }
